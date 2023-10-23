350b0bf56ba867264ebe6e35038735d0eaa4f182
burakkrt <burak.kurt@glomil.com>
2023-10-23 14:04:23 +0300

    created project

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Next JS Example',
+  authors: { name: '@burakkrt', url: 'https://github.com/burakkrt' },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="p-2">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus quos ipsa soluta amet?
+        Ratione.
+      </p>
+    </main>
+  );
+}
